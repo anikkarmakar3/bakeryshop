82cadb9e238b53ed23b91189ed6cbeb705d5a829
anikkarmakar3 <anikkarmakar3@gmail.com>
2021-12-01 22:55:53 +0300

    bakeryshop

--- app/src/main/java/com/example/bakeryshop/ForgetActivity.java
+++ app/src/main/java/com/example/bakeryshop/ForgetActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,36 +19,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.ref.Reference;
+
+public class ForgetActivity extends AppCompatActivity {
+
+
     DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://bakeryshop-6715f-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forget);
         final EditText username= findViewById(R.id.username);
         final EditText Password = findViewById(R.id.password);
-        final Button loginbtn= findViewById(R.id.login);
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        final Button resetbtn= findViewById(R.id.reset);
+        resetbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String usernametext=username.getText().toString();
                 final String passwordtext=Password.getText().toString();
                 if (usernametext.isEmpty() || passwordtext.isEmpty()){
                     Toast.makeText(getApplicationContext(),"please enter right information",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    databaseReference.child("consumer").addListenerForSingleValueEvent(new ValueEventListener() {
+                }else {
+                    databaseReference.child("consumer").addListenerForSingleValueEvent(new ValueEventListener(){
+
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
                             if (snapshot.hasChild(usernametext)){
                                 final String getPassword=snapshot.child(usernametext).child("password").getValue(String.class);
                                 if (getPassword.equals(passwordtext)){
-                                    Toast.makeText(getApplicationContext(),"successfully login",Toast.LENGTH_LONG).show();
-                                    startActivity(new Intent(MainActivity.this,MainActivity2.class));
-                                    finish();
+                                    Toast.makeText(getApplicationContext(),"Previous Password Is Same",Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(ForgetActivity.this,MainActivity.class));
+                                    /*finish();*/
                                 }
                                 else {
-                                    Toast.makeText(getApplicationContext(),"wrong credentials",Toast.LENGTH_LONG).show();
+                                    /*dataholder3 obj=new dataholder3(getPassword);*/
+                                    databaseReference.child("consumer").child(usernametext).child("password").setValue(passwordtext);
+                                    Toast.makeText(getApplicationContext(),"Reset Password Successfuly",Toast.LENGTH_LONG).show();
                                 }
                             }
                             else {
@@ -60,18 +67,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                 }
-            }
+        }
         });
-    }
-
-
-    public void create(View view) {
-        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-        startActivity(intent);
-    }
-
-    public void forget(View view) {
-        Intent intent=new Intent(MainActivity.this,ForgetActivity.class);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/bakeryshop/dataholder3.java
@@ -0,0 +1,17 @@
+package com.example.bakeryshop;
+
+public class dataholder3 {
+    String password;
+
+    public dataholder3(String password){
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
